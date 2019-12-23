8fa19ca8af95da066c70717eac25544d241185fe
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2019-12-23 14:07:01 +0800

    updating and deleting exercise

--- routes/exercises.js
+++ routes/exercises.js
@@ -18,5 +18,28 @@ router.get('/exercise', (req, res) => {
     // res.send(req.body)
 })
 
+router.get('/:id', (req, res) => {
+    Exercise.findById(req.params.id)
+        .then((exercise) => res.send(exercise))
+        .catch((error) => res.send(error))
+})
+
+router.delete('/:id', (req, res) => {
+    Exercise.findOneAndDelete(req.params.id)
+        .then(() => res.send('exercise deleted')).catch((error) => res.status(400).send(error))
+})
+
+router.patch('/update/:id', (req, res) => {
+    Exercise.findById(req.params.id)
+        .then((exercise) => {
+            exercise.name = req.body.name
+            exercise.description = req.body.description
+            exercise.duration = Number(req.body.duration)
+            exercise.date = Date.parse(req.body.date)
+            exercise.save()
+                .then(() => res.send('exercise updated')).catch((error) => res.send(error))
+        }).catch((error) => res.send(error))
+
+})
 
 module.exports = router

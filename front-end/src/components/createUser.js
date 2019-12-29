6711bc76124811c7ef9b7590166b74ad2cafb8e6
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2019-12-29 14:58:13 +0800

    create user ui

--- front-end/src/components/createUser.js
+++ front-end/src/components/createUser.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class CreateUser extends Component {
+
+    constructor(props) {
+        super(props)
+        this.onChangeUsername = this.onChangeUsername.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    state = { username: '' }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+    onSubmit(e) {
+        e.preventDefault()
+        const user = {
+            username: this.state.username
+        }
+        console.log(user)
+        this.setState({ username: '' })
+    }
     render() {
         return (
             <div>
-                <p> Create user  component</p>
+                <form onSubmit={this.state.onSubmit}>
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input type="text" required className="form-control"
+                            value={this.state.username} onChange={this.onChangeUsername} />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">Create User</button>
+                    </div>
+                </form>
             </div>
         )
     }

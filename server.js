b9423b5b1b84e299abe6df8ef55a68237a7005eb
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-01-06 12:29:31 +0800

    serving static

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.use(express.json());
 app.use(usersRouter);
 app.use(exercisesRouter);
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static("front-end/build"))
-// }
-app.use(express.static(path.join(__dirname, "front-end", "build")))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("front-end/build"))
+}
+// app.use(express.static(path.join(__dirname, "front-end", "build")))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "front-end", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"));
 });
 app.listen(port, () => {
     console.log(`App is running on port ${port}`)

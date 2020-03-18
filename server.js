2cefb6cd912773e4a35ec6e14e8db7f304a359d1
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-03-18 23:57:43 +0800

    static client

--- server.js
+++ server.js
@@ -11,15 +11,12 @@ const port = process.env.port || 5000;
 // const publicPath = path.join(__dirname, '..', './front-end/public')
 
 // app.use(express.static(publicPath))
-app.use(express.static("./client/build"))
+app.use(express.static(path.join("./client/build")))
 
 app.use(cors());
 app.use(express.json());
 app.use(usersRouter);
 app.use(exercisesRouter);
-
-
-// app.use(express.static(path.join(__dirname, "front-end", "build")))
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));

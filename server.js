b1ee786d7bc241a6862978faf6d806f242dd054b
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-03-19 10:26:53 +0800

    setting the main to server not index

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ const app = express();
 const port = process.env.port || 5000;
 
 
-// app.use(express.static(path.join("build")))
+app.use(express.static(path.join("build")))
 
 app.use(cors());
 app.use(express.json());
 app.use(usersRouter);
 app.use(exercisesRouter);
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 app.listen(port, () => {
     console.log(`App is running on port ${port}`)
 });
